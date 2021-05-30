c7ebaacbe59ca75a4e13a55002c206e56ac434fb
Anusha <anusharg85@gmail.com>
2021-05-29 19:15:45 -0700

    changed brackets

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.*;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,49 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
-
-
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void twoBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+    @Test
+    public void curlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("{[]}"));
+    }
+    @Test
+    public void oneLeftBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void oneRightBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void doubleNestedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+    @Test
+    public void stringInsideBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[string]"));
+    }
+    @Test
+    public void twoNestedBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[][]]"));
+    }
+    @Test
+    public void threeNestedBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[][][]]"));
+    }
+    @Test
+    public void fourNestedBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[][][][]]"));
+    }
+    @Test
+    public void twoNestedCurlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("{[][]}"));
+    }
 }
+
